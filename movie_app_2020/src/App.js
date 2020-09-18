72d93f4aefbbd753dacb4dec10ab38796390dca1
bp4sp4 <bp4sp4@naver.com>
2020-09-18 16:31:55 +0900

    9월 18 일 수업내용 업로드

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}){
+    console.log(fav)
+    return (
+    <h1>I love {fav}</h1>
+    );
+}
+const foodLink = [
+    {
+        name: '김치',
+        image : 'URL'
+    }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+    <div>
+    <h1>Hello</h1>  
     </div>
-  );
+    );
 }
 
 export default App;
