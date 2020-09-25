fa8797427116f0d201b25d2d5a2a0c73b693fe2a
bp4sp4 <bp4sp4@naver.com>
2020-09-25 16:48:31 +0900

    9월25일 수업내용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React from 'react';
+import imgA from './images/1.jpg';
+import imgB from './images/2.jpg';
+import imgC from './images/3.jpg';
+import imgD from './images/4.jpg';
+import imgE from './images/5.jpg';
 
-function Food({fav}){
-    console.log(fav)
+function Food({name, image, altText}){
     return (
-    <h1>I love {fav}</h1>
+        <div>
+    <h2>I love {name}</h2>
+    <img src ={image} alt= {altText} />
+    </div>
     );
 }
-const foodLink = [
+const foodILink = [
     {
-        name: '김치',
-        image : 'URL'
+        id : 1,
+        name: '돈까스',
+        image : imgA,
+        altText : '돈까스를 먹는 모습'
+    },
+    {
+        id : 2,
+        name: '족발',
+        image : imgB,
+        altText : '햄버거를 먹는 모습'
+    },
+    {
+        id : 3,
+        name: '쌀국수',
+        image : imgC,
+        altText : '쌀국수를 먹는 모습'
+    },
+    {
+        id : 4,
+        name: '감자튀김',
+        image : imgD,
+        altText : '감자튀김을 먹는 모습'
+    },
+    {
+        id : 5,
+        name: '튀김',
+        image : imgE,
+        altText : '튀김을 먹는 모습'
     }
-];
+    ];
 
 function App() {
-return (
-    <div>
-    <h1>Hello</h1>  
-    </div>
+return(
+<div>
+    {foodILink.map(dish => (<Food  key = {dish.id}  name={dish.name} image={dish.image} altText={dish.altText} />))}
+</div>
     );
 }
 
