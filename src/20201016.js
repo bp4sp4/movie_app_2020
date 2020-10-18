c43a3e735b36637d236cd5dad775e2b4072c8b38
bp4sp4 <bp4sp4@naver.com>
2020-10-18 12:16:33 +0900

    1.월 16일

---
+++ src/20201016.js
@@ -0,0 +1,43 @@
+/*import React from'react';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        console.log('hello');
+    }
+
+    add = () => {
+        this.setState(current => ({
+            count : current.count + 1,
+        }));
+    };
+    minus = () => {
+        this.setState(current => ({
+            count : current.count - 1,
+        }));
+    };
+    componentDidMount() {
+        console.log('componentDidMoun');
+    }
+    componentDidUpdate() {
+        console.log('componentDidMoun');
+    }
+    componentWillUnmount() {
+        console.log('componentDidMoun')
+    }
+    
+    render() {
+        console.log('render');
+     return (
+         <div>
+     <h1>The number is: {this.state.count}</h1>
+     <button onClick={this.add}>Add</button>
+     <button onClick={this.minus}>Minus</button>
+     </div>
+        );
+    }
+ }
+
+
+export default App;
+*/
--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from'react';
+import axios from 'axios';
 
 class App extends React.Component {
     state = {
-        count : 0
+        isLoading : true,
+        movies : [],
     }
-
-    add = () => {
-        this.setState(current => ({
-            count : current.count + 1,
-        }));
-    };
-    minus = () => {
-        this.setState(current => ({
-            count : current.count - 1,
-        }));
-    };
-    render() {
-     return (
-         <div>
-     <h1>This number is: {this.state.count}</h1>
-     <button onClick={this.add}>Add</button>&nbsp;&nbsp;&nbsp;
-     <button onClick={this.minus}>Minus</button>
-     </div>
-     );
+    getMovies = async () => {
+        const movies = await  axios.get('https://yts-proxy.now.sh/list_movies.json');
+    }
+    componentDidMount() {
+        // 영화 데이터 로딩
+            this.getMovies();
+        }
+    render(){
+        const {isLoading} = this.state;
+        return(
+            <div>
+                {isLoading ? 'Loding...' : 'ready!!!'}
+            </div>
+        );
     }
 }
+
 
 export default App;
